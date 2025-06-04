055a42fb950b2c803050887d20b78f5bdc4f8a14
cubie <64768472+Cubie87@users.noreply.github.com>
2025-06-04 21:50:42 +0930

    blink into blink + I2C Address Scanner

--- test-modules/esp32-blink/src/main.cpp
+++ test-modules/esp32-blink/src/main.cpp
@@ -1,26 +1,77 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+// SPDX-FileCopyrightText: 2023 Carter Nelson for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+// --------------------------------------
+// i2c_scanner
+//
+// Modified from https://playground.arduino.cc/Main/I2cScanner/
+// --------------------------------------
+
+#include <Wire.h>
 #include "Arduino.h"
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN 2
 
-void setup()
-{
-  // initialize LED digital pin as an output.
+// Set I2C bus to use: Wire, Wire1, etc.
+#define WIRE Wire
+
+void setup() {
+  WIRE.begin();
+
+  Serial.begin(9600);
+  while (!Serial)
+     delay(10);
+  Serial.println("\nI2C Scanner");
+
+  
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
+
+void loop() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+
+
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
 
-  // wait for a second
   delay(1000);
 
   // turn the LED off by making the voltage LOW
